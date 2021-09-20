f5f62f2386432ef2e82bae4a4e7f7565cf72d28d
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2021-09-20 20:11:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -360,8 +360,8 @@ async function tradeCycle() {
                     'Tentando executá-lo com um possível stop loss',
                   );
                   bot.telegram.sendMessage(botchat,
-                    `[${tradeCycleCount}] Only the first leg of the arbitrage was executed. ` +
-                    'Trying to execute it at a possible loss.'
+                    `[${tradeCycleCount}] Apenas a primeira etapa da arbitragem foi executada.` +
+                    'Tentando executá-lo com um possível stop loss',
                   );
                   secondLeg = await bc.offer({
                     amount,
